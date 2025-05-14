f810b39d894515945fe1a5c078fcdda43e97d78f
rubenkristian96 <rubenkristian96@gmail.com>
2025-05-15 02:16:05 +0700

    add component event handller

--- discord-bot/component.go
+++ discord-bot/component.go
@@ -1,1 +1,11 @@
 package discordbot
+
+import "github.com/disgoorg/disgo/events"
+
+func (db *DiscordBot) ComponentSelectGroup(event *events.ComponentInteractionCreate) {
+	// event.Client().Rest().AddMemberRole(*event.GuildID, )
+}
+
+func (db *DiscordBot) ComponentSelectTime(event *events.ComponentInteractionCreate) {
+	// event.Client().Rest().AddMemberRole(*event.GuildID, )
+}
